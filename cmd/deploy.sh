1b6b9a082900412150063fc2036d9259330f9d4a
Martin Zöller <m325z.dev@pm.me>
2025-04-18 10:13:26 +0700

    Update deploy script to include environment variables

--- cmd/deploy.sh
+++ cmd/deploy.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 gcloud run deploy go-http-function \
     --source . \
     --function ComposeHaiku \
     --base-image go123 \
     --region europe-north2 \
+    --env-vars-file=env.yaml \
     --allow-unauthenticated
